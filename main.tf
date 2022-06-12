33a5c4b0307174887d31e57067b677b6c9e0c627
Ubuntu <ubuntu@ip-172-31-27-79.ec2.internal>
2022-06-12 03:30:20 +0000

    change all

--- main.tf
+++ main.tf
@@ -54,95 +54,95 @@ resource "random_shuffle" "public_az" {
   result_count = var.max_subnets
 }
 
-resource "aws_vpc" "mtc_vpc" {
+resource "aws_vpc" "lunx_vpc" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
   enable_dns_support   = true
 
   tags = {
-    Name = "mtc_vpc-${random_integer.random.id}"
+    Name = "lunx_vpc-${random_integer.random.id}"
   }
   lifecycle {
     create_before_destroy = true
   }
 }
 
-resource "aws_subnet" "mtc_public_subnet" {
+resource "aws_subnet" "lunx_public_subnet" {
   count                   = var.public_sn_count
-  vpc_id                  = aws_vpc.mtc_vpc.id
+  vpc_id                  = aws_vpc.lunx_vpc.id
   cidr_block              = var.public_cidrs[count.index]
   map_public_ip_on_launch = true
   availability_zone       = random_shuffle.public_az.result[count.index]
 
   tags = {
-    Name = "mtc_public_${count.index + 1}"
+    Name = "lunx_public_${count.index + 1}"
   }
 }
 
 resource "aws_subnet" "mtc_private_subnet" {
   count                   = var.private_sn_count
-  vpc_id                  = aws_vpc.mtc_vpc.id
+  vpc_id                  = aws_vpc.lunx_vpc.id
   cidr_block              = var.private_cidrs[count.index]
   map_public_ip_on_launch = false
   availability_zone       = random_shuffle.public_az.result[count.index]
 
   tags = {
-    Name = "mtc_private_${count.index + 1}"
+    Name = "lunx_private_${count.index + 1}"
   }
 }
 
-resource "aws_db_subnet_group" "mtc_rds_subnetgroup" {
+resource "aws_db_subnet_group" "lunx_rds_subnetgroup" {
   count      = var.db_subnet_group == "true" ? 1 : 0
-  name       = "mtc_rds_subnetgroup"
-  subnet_ids = aws_subnet.mtc_private_subnet.*.id
+  name       = "lunx_rds_subnetgroup"
+  subnet_ids = aws_subnet.lunx_private_subnet.*.id
   tags = {
-    Name = "mtc_rds_sng"
+    Name = "lunx_rds_sng"
   }
 }
 
-resource "aws_internet_gateway" "mtc_internet_gateway" {
-  vpc_id = aws_vpc.mtc_vpc.id
+resource "aws_internet_gateway" "lunx_internet_gateway" {
+  vpc_id = aws_vpc.lunx_vpc.id
 
   tags = {
-    Name = "mtc_igw"
+    Name = "lunx_igw"
   }
 }
 
-resource "aws_route_table" "mtc_public_rt" {
-  vpc_id = aws_vpc.mtc_vpc.id
+resource "aws_route_table" "lunx_public_rt" {
+  vpc_id = aws_vpc.lunx_vpc.id
 
   tags = {
-    Name = "mtc_public"
+    Name = "lunx_public"
   }
 }
 
 
 resource "aws_route" "default_route" {
-  route_table_id         = aws_route_table.mtc_public_rt.id
+  route_table_id         = aws_route_table.lunx_public_rt.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.mtc_internet_gateway.id
+  gateway_id             = aws_internet_gateway.lunx_internet_gateway.id
 }
 
 
-resource "aws_default_route_table" "mtc_private_rt" {
-  default_route_table_id = aws_vpc.mtc_vpc.default_route_table_id
+resource "aws_default_route_table" "lunx_private_rt" {
+  default_route_table_id = aws_vpc.lunx_vpc.default_route_table_id
 
   tags = {
-    Name = "mtc_private"
+    Name = "lunx_private"
   }
 }
 
-resource "aws_route_table_association" "mtc_public_assoc" {
+resource "aws_route_table_association" "lunx_public_assoc" {
   count          = var.public_sn_count
-  subnet_id      = aws_subnet.mtc_public_subnet.*.id[count.index]
-  route_table_id = aws_route_table.mtc_public_rt.id
+  subnet_id      = aws_subnet.lunx_public_subnet.*.id[count.index]
+  route_table_id = aws_route_table.lunx_public_rt.id
 }
 
-resource "aws_security_group" "mtc_sg" {
+resource "aws_security_group" "lunx_sg" {
   for_each    = local.security_groups
   name        = each.value.name
   description = each.value.description
-  vpc_id      = aws_vpc.mtc_vpc.id
+  vpc_id      = aws_vpc.lunx_vpc.id
 
 
 
--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,21 @@
 # --- networking/outputs.tf ---
 
 output "vpc_id" {
-  value = aws_vpc.mtc_vpc.id
+  value = aws_vpc.lunx_vpc.id
 }
 
 output "db_subnet_group_name" {
-  value = aws_db_subnet_group.mtc_rds_subnetgroup.*.name
+  value = aws_db_subnet_group.lunx_rds_subnetgroup.*.name
 }
 
 output "db_security_group" {
-  value = aws_security_group.mtc_sg["rds"].id
+  value = aws_security_group.lunx_sg["rds"].id
 }
 
 output "public_sg" {
-  value = aws_security_group.mtc_sg["public"].id
+  value = aws_security_group.lunx_sg["public"].id
 }
 
 output "public_subnets" {
-  value = aws_subnet.mtc_public_subnet.*.id
+  value = aws_subnet.lunx_public_subnet.*.id
 }
